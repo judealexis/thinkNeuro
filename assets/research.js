6820a1a20d48f95829421d8141e148b4250ae29d
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2024-03-27 14:17:11 -0700

    added this years research cohort

---
+++ assets/research.js
@@ -0,0 +1,46 @@
+let posters = "";
+let posterNames = [
+    "Examining Linkages of ADHD Risk Allele Dopamine Transporter Gene DAT1: A Bibliometric Analysis", 
+    "Examining the Correlation Between Lifestyle Factors and the Clinical Presentation of Multiple Sclerosis",
+    "Levodopa as a Treatment for Parkinson’s Disease",
+    "A Study of Artificial Intelligence Papers within Neuroscience Research",
+    "Opioid Epidemic Policy: Predictive Power of Collaborative Quantity vs. Quality in Determining Impactful Research",
+    "Reading between the lines: Gaps in Health Outcomes as a Result of Varying Health Literacy Levels",
+    "Nanoparticles as a Drug Delivery Method",
+    "Investigating Psychedelic-Assisted Psychotherapy and PTSD",
+    "Exploring Gaps, Trends and Applications of Research on Computational Models of Memory and Learning",
+    "Trends in Disparity Research in Recent Telemedicine Literature",
+    "Impact and Optimization of BCIs on Neuroplasticity for Rehabilitation in Neurological Disorders",
+    "Therapeutic Benefits of Deep Brain Stimulation in Neurodegenerative Diseases Based on the Top 100 Cited Literature",
+    "Exploring the Use of Spinal Cord Stimulation: A Review of its Applications for Chronic Pain Management",
+    "Autism Spectrum Disorder: Learning & Social Behavior of Children with ASD During COVID-19",
+    "Disparities Between Zip Codes on the Spread, Diagnosis, Treatment, and Vaccination of COVID-19",
+    "Neuroimaging: Amyloid PET Scans For Alzheimer’s Diagnosis",
+    "Alzheimer’s Disease",
+    "Decoding Brain Waves: Machine Learning for Epilepsy Detection via EEG Scans",
+    "Universal Healthcare: Private vs. Public Systems and Policy Implementation"
+];
+
+for (i = 1; i <= posterNames.length; i++){
+    posters += 
+    `
+    <h3>${posterNames[i-1]}</h3> 
+    <div class="sole-project-container">
+        <div class="project-item">
+            <object
+            data='assets/posters/2024Cohort/2024poster${i}.pdf#view=FitH'
+            type="application/pdf"
+            id="pdfPoster">
+                <iframe
+                src='assets/posters/2024Cohort/2024poster${i}.pdf#view=FitH'
+                id="pdfPoster">
+                    <p>This browser does not support PDF!</p>
+                </iframe>
+            </object>
+        </div>
+    </div>
+    `
+}
+
+
+document.getElementById("insert_cohort_here").innerHTML = posters;
